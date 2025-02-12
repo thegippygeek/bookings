731108d6e26a924843b3675e1359fb41c42376a6
Tony Hallworth <tonyh@tgg-m1.local>
2025-02-12 19:52:42 +1100

    pre server

---
+++ migrations/20250212084757_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users
+(id, first_name, last_name, email, "password", access_level, created_at, updated_at)
+VALUES(1, 'Tony', 'Hallworth', 'tony.h@here.com', '$2a$12$tuOAiiRW1eGyL5SwhHqaIu.nr4a2oZsMrQqYXsLL3hvOVyhcU9u5C', 3, '2025-02-10 00:00:00.000', '2025-02-10 00:00:00.000');
